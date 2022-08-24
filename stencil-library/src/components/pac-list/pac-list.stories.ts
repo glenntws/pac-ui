3158e71453013b77b56061169b03af8859c40c86
Glenn Toews <glenntoews@outlook.com>
2022-08-24 22:19:29 +0200

    Added initially generated variants of first planned component set and implemented progress bar

--- stencil-library/src/components/pac-list/pac-list.stories.ts
+++ stencil-library/src/components/pac-list/pac-list.stories.ts
@@ -7,23 +7,29 @@ export default {
 };
 
 const Template = () => `
-<pac-list>
-    <pac-list-header>
-        Demo list
-    </pac-list-header>
-    
-    <pac-list-item>
-        <pac-label>First item</ion-label>
-    </pac-list-item>
-    <pac-list-item>
-        <pac-label position="fixed">Last name</pac-label>
-        <pac-input placeholder="Mustermann"></pac-input>
-    </pac-list-item>
-    <pac-list-item>
-        <pac-label position="stacked">Interests</pac-label>
-        <pac-input placeholder="Skiing"></pac-input
-    </pac-list-item>
-</pac-list>
+<pac-container look="solid" color="slate">
+    <pac-list>
+        <pac-list-header>
+            Demo list
+        </pac-list-header>
+        
+        <pac-list-item>
+            <pac-label>First item</ion-label>
+        </pac-list-item>
+        <pac-list-item>
+            <pac-label position="fixed">Last name</pac-label>
+            <pac-input placeholder="Mustermann"></pac-input>
+        </pac-list-item>
+        <pac-list-item>
+            <pac-label position="stacked">Interests</pac-label>
+            <pac-input placeholder="Skiing"></pac-input
+        </pac-list-item>
+        <pac-list-item>
+            <pac-label >Interests</pac-label>
+            <pac-input placeholder="Skiing"></pac-input
+        </pac-list-item>
+    </pac-list>
+</pac-container>
 `;
 
 export const Demo = Template.bind({});
